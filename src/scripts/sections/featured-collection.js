b5b2bdc5886ca0fa599a454fb8ab65113d0597e3
Usef-BH <contact@meraki-apps.com>
2020-08-05 19:58:23 +0100

    import siwper

--- src/scripts/sections/featured-collection.js
+++ src/scripts/sections/featured-collection.js
@@ -6,9 +6,9 @@
  *
  * @namespace featuredCollection
  */
-import {register} from '@shopify/theme-sections';
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.css';
+import { register } from "@shopify/theme-sections";
+import Swiper from "swiper";
+import "swiper/swiper-bundle.css";
 
 /**
  * Featured collection constructor
@@ -16,109 +16,111 @@ import 'swiper/swiper-bundle.css';
  *
  * @param {string} container - selector for the section container DOM element
  */
-register('featured-collection', {
+register("featured-collection", {
   init() {
     this.publicMethod();
   },
 
-  publicMethod() {
-    window.console.log('Initialising featured collection section');
-  },
-});
-
-new Swiper('.swiper-container', {
-  slidesPerView: 4,
-  slidesPerGroup: 4,
-  spaceBetween: 22,
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-      spaceBetween: 22,
-    },
-    // when window width is >= 480px
-    640: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-      spaceBetween: 22,
-    },
-    // when window width is >= 640px
-    960: {
-      slidesPerView: 3,
-      slidesPerGroup: 3,
-      spaceBetween: 22,
-    },
-    1280: {
+  onLoad: function() {
+    new Swiper(".swiper-container", {
       slidesPerView: 4,
       slidesPerGroup: 4,
       spaceBetween: 22,
-    },
-  },
-  pagination: {
-    el: '.swiper-pagination',
-  },
-});
-
-const buttons = document.querySelectorAll('#add-to-cart');
-
-[...buttons].forEach((button) => {
-  button.addEventListener('click', (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-
-    const variantId = button.dataset.variantId;
-
-    const cartText = button.querySelector('.add-to-cart-text');
-    const cartLoader = button.querySelector('.add-to-cart-loader');
-
-    cartText ? cartText.classList.add('hidden') : null;
-    cartLoader ? cartLoader.classList.remove('hidden') : null;
-
-    fetch('/cart/add.js', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+          spaceBetween: 22
+        },
+        // when window width is >= 480px
+        640: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          spaceBetween: 22
+        },
+        // when window width is >= 640px
+        960: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+          spaceBetween: 22
+        },
+        1280: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+          spaceBetween: 22
+        }
       },
-      body: JSON.stringify({
-        items: [
-          {
-            quantity: 1,
-            id: variantId,
+      pagination: {
+        el: ".swiper-pagination"
+      }
+    });
+
+    const buttons = document.querySelectorAll("#add-to-cart");
+
+    [...buttons].forEach(button => {
+      button.addEventListener("click", event => {
+        event.stopPropagation();
+        event.preventDefault();
+
+        const variantId = button.dataset.variantId;
+
+        const cartText = button.querySelector(".add-to-cart-text");
+        const cartLoader = button.querySelector(".add-to-cart-loader");
+
+        cartText ? cartText.classList.add("hidden") : null;
+        cartLoader ? cartLoader.classList.remove("hidden") : null;
+
+        fetch("/cart/add.js", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
           },
-        ],
-      }),
-    })
-      .then((resp) => {
-        if (resp.ok) {
-          return resp.json();
-        } else {
-          throw Error('Adding to cart failed!');
-        }
-      })
-      .then((resp) => {
-        const cartItemCount = document.querySelector('#cart-item-count');
+          body: JSON.stringify({
+            items: [
+              {
+                quantity: 1,
+                id: variantId
+              }
+            ]
+          })
+        })
+          .then(resp => {
+            if (resp.ok) {
+              return resp.json();
+            } else {
+              throw Error("Adding to cart failed!");
+            }
+          })
+          .then(resp => {
+            const cartItemCount = document.querySelector("#cart-item-count");
 
-        if (cartItemCount) {
-          const oldCartCount = Number(cartItemCount.innerText);
-          cartItemCount.innerText = oldCartCount + 1;
-        }
+            if (cartItemCount) {
+              const oldCartCount = Number(cartItemCount.innerText);
+              cartItemCount.innerText = oldCartCount + 1;
+            }
 
-        cartText.innerText = 'Thank You!';
-        cartLoader.classList.add('hidden');
-        cartText.classList.remove('hidden');
-        setTimeout(() => {
-          cartText.innerText = 'Add to cart';
-        }, 3500);
-      })
-      .catch((err) => {
-        console.error('[ERROR]:', err);
-        cartText.innerText = 'Operation Failed!';
-        cartLoader.classList.add('hidden');
-        cartText.classList.remove('hidden');
-        setTimeout(() => {
-          cartText.innerText = 'Add to cart';
-        }, 3500);
+            cartText.innerText = "Thank You!";
+            cartLoader.classList.add("hidden");
+            cartText.classList.remove("hidden");
+            setTimeout(() => {
+              cartText.innerText = "Add to cart";
+            }, 3500);
+          })
+          .catch(err => {
+            console.error("[ERROR]:", err);
+            cartText.innerText = "Operation Failed!";
+            cartLoader.classList.add("hidden");
+            cartText.classList.remove("hidden");
+            setTimeout(() => {
+              cartText.innerText = "Add to cart";
+            }, 3500);
+          });
       });
-  });
+    });
+  },
+
+  publicMethod() {
+    window.console.log("Initialising featured collection section");
+  }
 });
